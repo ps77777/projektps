36a261cff11a89921635beb0ce72e8278c83436f
stasik <stasik@wszib.edu.pl>
2019-05-12 11:13:03 +0200

    szkielet projektu

---
+++ src/main/java/pl/edu/wszib/projektps/ProjektpsApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.projektps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjektpsApplication.class, args);
+    }
+
+}
